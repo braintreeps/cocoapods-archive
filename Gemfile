b7079d14155678fc7910d98f02978990d9370f21
Braintree <code@getbraintree.com>
2014-07-11 13:47:39 -0700

    Hello world

--- Gemfile
+++ Gemfile
@@ -2,3 +2,12 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in cocoapods-archive.gemspec
 gemspec
+
+group :development do
+  gem 'cocoapods',    :git => "https://github.com/CocoaPods/CocoaPods.git", :branch => 'master'
+  gem 'claide',       :git => "https://github.com/CocoaPods/CLAide.git", :branch => 'master'
+  # gem 'bacon'
+  # gem 'mocha-on-bacon'
+  # gem 'mocha'
+  # gem 'prettybacon'
+end
--- lib/pod/command/spec/archive.rb
+++ lib/pod/command/spec/archive.rb
@@ -1,20 +1,18 @@
 module Pod
   class Command
-    class Spec
+    class Lib
       # This is an example of a cocoapods plugin adding a subcommand to
       # the 'pod spec' command. Adapt it to suit your needs.
       #
       # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
       #       in the `plugins.json` file, once your plugin is released.
       #
-      class Archive < Spec
-        self.summary = "Short description of cocoapods-archive."
+      class Archive < Lib
+        self.summary = "Archives your pod in as a static library."
 
         self.description = <<-DESC
           Longer description of cocoapods-archive.
         DESC
-
-        self.arguments = 'NAME'
 
         def initialize(argv)
           @name = argv.shift_argument
@@ -27,9 +25,7 @@ module Pod
         end
 
         def run
-          path = get_path_of_spec(@name)
-          spec = Specification.from_file(path)
-          UI.puts "Hello #{spec.name}"
+          UI.puts "Hello, world"
         end
       end
     end
